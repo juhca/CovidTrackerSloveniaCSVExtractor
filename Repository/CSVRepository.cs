21be5932fcc21da6de6df427607217d9bcefa2e8
Tomas Štefe <tomas.stefe@gmail.com>
2023-03-20 18:05:05 +0100

    Deleted unused code

--- Repository/CSVRepository.cs
+++ Repository/CSVRepository.cs
@@ -36,24 +36,12 @@ namespace IndigoLabs2.Repository
             return await ApplicationContext.CSVRegions.OrderByDescending(x => x.Date).Take(7).ToListAsync();
         }
 
-
-        //var tmp = output.Where(x => DateTime.Parse(((IDictionary<string, object>)x)["date"].ToString()) >= dateFrom)
-        //                    .Where(x => DateTime.Parse(((IDictionary<string, object>)x)["date"].ToString()) <= dateTo)
-        //                    .ToList();
-
         async Task<List<CSVRegionCases>> ICSVRepository.GetRecordsByDate(DateTime dateTo, DateTime dateFrom)
         {
             List<CSVRegionCases> records = await ApplicationContext.CSVRegions
                 .Where(x => x.Date >= dateFrom && x.Date <= dateTo)
                 .ToListAsync();
             return records;
-        }
-
-
-        //async Task IUserRepository.DeleteAllUsers()
-        //{
-        //    await ApplicationContext.Users.ForEachAsync(x => ApplicationContext.Users.Remove(x));
-        //    await ApplicationContext.SaveChangesAsync();
-        //}    
+        }  
     }
 }
